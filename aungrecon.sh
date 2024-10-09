efceedfb4d6af24c50e980bcaa507865ad8c1917
Aung San Oo <56332066+aungsanoo-usa@users.noreply.github.com>
2024-10-09 00:40:37 -0400

    Add files via upload

---
+++ aungrecon.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+yellow="\e[0;33m"
+green="\e[0;92m"
+cyan="\e[0;36m"
+uline="\e[0;35m"
+reset="\e[0m"
+
+printf "\n${yellow}Welcome to Aung Recon main script
+#              ╭━━━╮╱╱╱╱╱╱╱╱╱╭━━━╮
+#              ┃╭━╮┃╱╱╱╱╱╱╱╱╱┃╭━╮┃
+#              ┃┃╱┃┣╮╭┳━╮╭━━╮┃╰━╯┣━━┳━━┳━━┳━╮
+#              ┃╰━╯┃┃┃┃╭╮┫╭╮┃┃╭╮╭┫┃━┫╭━┫╭╮┃╭╮╮
+#              ┃╭━╮┃╰╯┃┃┃┃╰╯┃┃┃┃╰┫┃━┫╰━┫╰╯┃┃┃┃
+#              ╰╯╱╰┻━━┻╯╰┻━╮┃╰╯╰━┻━━┻━━┻━━┻╯╰╯
+#              ╱╱╱╱╱╱╱╱╱╱╭━╯┃
+#              ╱╱╱╱╱╱╱╱╱╱╰━━╯  aungsanoo.com${reset}\n"
+
+
+printf "\n${yellow}###############################${reset}\n"
+V_MY_PATH=$HOME
+# Ask the user for the website URL or domain
+read -p "[+]Enter the website domain:" website_input
+
+# Normalize the input: Add "https://" if the input is just a domain without protocol
+if [[ ! $website_input =~ ^https?:// ]]; then
+    website_url="https://$website_input"
+else
+    website_url="$website_input"
+fi
+
+# Inform the user of the normalized URL being used
+echo -e "${blue}[+]Normalized URL being used${reset}: $website_url"
+# Create an output directory if it doesn't exist
+output_dir="output"
+mkdir -p "$output_dir"
+printf "${uline}#######################################################################${reset}\n"
+#Sundomain
+echo -e "${yellow}\e[5m[+]Findimg Subdomain......${reset}"
+printf "${uline}#######################################################################${reset}\n"
+subfinder -d $website_input -all -recursive > "sub.txt" 
+printf "${uline}#######################################################################${reset}\n"
+echo -e "${yellow}\e[5m[+] Filtering Alive Sundomains....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+#And then we gona check which subdomain are alive using https tool
+cat "sub.txt" | httpx -v > "alivesub.txt"
+printf "${uline}#######################################################################${reset}\n"
+#takeover
+echo -e "${yellow}\e[5m[+] check subdomain takeover....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+subzy run --targets "sub.txt"
+printf "${uline}#######################################################################${reset}\n"
+# SQLi
+echo -e "${yellow}\e[5m[+]Finding SQLI vulnerability....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+paramspider -l "alivesub.txt" -s
+cd results
+cat *.txt > allurls.txt
+#Remove FUZZ and save as final.txt
+cat allurls.txt | sed 's/FUZZ//g' > final.txt
+mv final.txt $HOME/aungrecon/output/final.txt
+cd $HOME/aungrecon/bsqli
+python3 main.py -l $HOME/aungrecon/output/final.txt -p payloads/xor.txt -t 5
+cp output.txt $HOME/aungrecon/output/sqli_vul.txt
+printf "${uline}#######################################################################${reset}\n"
+# XSS
+echo -e "${yellow}\e[5m[+]Finding XSS vulnerability....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+cd $HOME/aungrecon/xss_vibes
+python3 main.py -f $HOME/aungrecon/output/final.txt -t 7 -o $HOME/aungrecon/output/xss_vul.txt.txt
+printf "${uline}#######################################################################${reset}\n"
+
+#OprnRedirect
+echo -e "${yellow}\e[5m[+] Open Redirect Testing ....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+
+cat $HOME/aungrecon/results/allurls.txt |  openredirex -p $HOME/aungrecon/or.txt -k "FUZZ" -c 30 > $HOME/aungrecon/open_redirect_vul.txt
+
+# LFI
+printf "${uline}#######################################################################${reset}\n"
+echo -e "${yellow}\e[5m[+]Finding LFI vulnerability....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+cd ..
+echo $website_input | gau | gf lfi | uro | sed 's/=.*/=/' | qsreplace "FUZZ" | sort -u | xargs -I{} ffuf -u {} -w lfi.txt -c -mr "root:x:0:" -v > $HOME/aungrecon/output/lfi_vul.txt
+printf "${uline}#######################################################################${reset}\n"
+echo -e "${yellow}\e[5m[+] Remove the intermediate output files ....${reset}"
+printf "${uline}#######################################################################${reset}\n"
+mv $HOME/aungrecon/open_redirect_vul.txt $HOME/aungrecon/output/open_redirect_vul.txt
+rm sub.txt alivesub.txt
+rm -r results
+cd bsqli
+rm *.txt
+cd ..
+cd output
+rm final.txt
+
+printf "${uline}#######################################################################${reset}\n"
+# Notify the user that all tasks are complete
+echo -e "${yellow}\e[5mFiltered URLs have been saved to the respective output files in the 'output' directory:${reset}"
+echo -e "${cyan}\e[5m- XSS: $output_dir/xss_vul.txt${reset}"
+echo -e "${cyan}\e[5m- Open Redirect: $output_dir/open_redirect_vul.txt${reset}"
+echo -e "${cyan}\e[5m- LFI: $output_dir/lfi_vul.txt${reset}"
+echo -e "${cyan}\e[5m- SQLi: $output_dir/sqli_vul.txt${reset}"
+printf "${uline}#######################################################################${reset}\n"
---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+BOLD=$(tput bold)
+YELLOW=$(tput setaf 3)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+NORMAL=$(tput sgr0)
+
+printf "${BOLD}${YELLOW}##########################################################\n"
+printf "##### Welcome to the AungRecon dependency installer #####\n"
+printf "##########################################################\n\n${NORMAL}"
+
+sudo apt-get -y update
+
+printf "${BOLD}${MAGENTA}Installing programming languages and essential packages\n${NORMAL}"
+sudo apt-get install -y python3-pip dnspython golang
+
+printf "${BOLD}${MAGENTA}Cloning repositories and installing dependencies\n${NORMAL}"
+cd $HOME/aungrecon
+
+declare -A REPOS=(
+  ["xss_vibes"]="https://github.com/faiyazahmad07/xss_vibes.git"
+  ["Paramspider"]="https://github.com/devanshbatham/paramspider"
+  ["bsqli"]="https://github.com/KKonaNN/bsqli.git"
+  ["openredirex"]="https://github.com/devanshbatham/openredirex"
+  ["Gf-Patterns"]="https://github.com/1ndianl33t/Gf-Patterns"
+)
+
+for repo in "${!REPOS[@]}"; do
+  printf "${CYAN}Cloning ${repo}\n${NORMAL}"
+  git clone "${REPOS[$repo]}"
+  cd "$repo"
+  if [ -f requirements.txt ]; then
+    pip3 install -r requirements.txt --break-system-packages
+  fi
+  cd ..
+ 
+done
+
+
+pip3 install uro
+
+pip3 install pystyle
+
+
+
+mkdir -p ~/.gf
+cp -r Gf-Patterns/* ~/.gf
+
+printf "${BOLD}${MAGENTA}Installing GO tools\n${NORMAL}"
+declare -a GO_TOOLS=(
+  "github.com/projectdiscovery/subfinder/v2/cmd/subfinder"
+  "github.com/projectdiscovery/httpx/cmd/httpx"
+  "github.com/lc/gau"
+  "github.com/tomnomnom/gf"
+  "github.com/tomnomnom/qsreplace"
+  "github.com/PentestPad/subzy"
+)
+
+for tool in "${GO_TOOLS[@]}"; do
+  printf "${CYAN}Installing $(basename $tool)\n${NORMAL}"
+  go install "$tool@latest"
+  sudo cp "$HOME/go/bin/$(basename $tool)" /usr/local/bin/
+done
+
+printf "${CYAN}Installing ffuf\n${NORMAL}"
+sudo apt install ffuf
+
+printf "${CYAN}openredirex\n${NORMAL}"
+cd $HOME/aungrecon/openredirex
+chmod +x setup.sh
+sudo bash setup.sh
+cd ..
+
+printf "${BOLD}${YELLOW}Installation completed successfully!\n${NORMAL}"
